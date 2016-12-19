56445b8d78b43307ef48bc73cd8d6a05b86b8df3
jbradley <jbradley@hubspot.com>
2016-12-19 18:36:07 -0500

    add product method

--- src/UberClient.js
+++ src/UberClient.js
@@ -1,5 +1,35 @@
 'use es6';
 
+import rp from 'request-promise';
+
+import Subpath from '../data/Subpath';
+
 export default class UberClient {
-  
+  constructor(serverToken) {
+    this.serverToken = serverToken;
+  }
+
+  getProducts(coordinates) {
+    return execute(Subpath.PRODUCTS, coordinates);
+  }
+
+  execute(subpath, coordinates) {
+    return rp(this.buildOptions(subpath, coordinates))
+              .then(result => console.log(result))
+              .catch(err => throw new Error(err));
+  }
+
+  buildOptions(subpath, coordinates) {
+    return {
+      uri: `https://api.uber.com/v1.2/${subpath.value}`,
+      qs: {
+        latitude: coordinates.latitude,
+        longitude: coordinates.longitude
+      },
+      headers: {
+        Authorization: `Bearer ${this.serverToken}`
+      },
+      json: true
+    };
+  }
 }
---
+++ src/data/Subpath.js
@@ -0,0 +1,10 @@
+'use es6';
+
+import {Enum} from "enumify";
+
+export default class Subpath extends Enum {}
+Subpath.initEnum({
+  PRODUCTS: {
+    value: 'products',
+  }
+});

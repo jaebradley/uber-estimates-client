e193886f43ce2e9b0619cf884fe4d1c24ec552c5
jaebradley <jae.b.bradley@gmail.com>
2017-06-15 01:04:14 -0400

    add UberError to export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 'use es6';
 
 import UberClient from './UberClient';
+import UberError from './errors/UberError';
 
 module.exports = {
-  UberClient: UberClient
+  UberClient: UberClient,
+  UberError: UberError
 };
